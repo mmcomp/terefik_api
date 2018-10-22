507f79dbe4f06122742779e1b92ca3ee0e74dbfe
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2018-10-22 13:37:50 +0330

    Sub games on version order

--- app/Controllers/Mqtt/SubGameController.js
+++ app/Controllers/Mqtt/SubGameController.js
@@ -1,25 +1,45 @@
 'use strict'
 
-const Code = use('App/Models/Code')
-const Transaction = use('App/Models/Transaction')
 const SubGame = use('App/Models/SubGame')
 const Moment = use('App/Libs/Moment')
 const Time = Moment.moment()
 
 const Env = use('Env')
-const Validations = use('App/Libs/Validations')
-const _ = require('lodash')
+const Database  = use('Database')
 
 class SubGameController {
   static async index (params, user) {
-    let subGames = await SubGame.all()
-    subGames = subGames.toJSON()
-    for(let i = 0;i < subGames.length;i++) {
-      subGames[i].icon_path = Env.get('SITE_URL') + subGames[i].icon_path
-      subGames[i].file_path = Env.get('SITE_URL') + subGames[i].file_path
-      delete subGames[i].updated_at
-      delete subGames[i].created_at
+    let subGames = await SubGame.all(), tmp, selectedIndex, subGameNames = []
+    let subGamesData = subGames.toJSON()
+    subGames = []
+
+    for(let i = 0;i < subGamesData.length;i++) {
+      tmp = subGamesData[i]
+      selectedIndex = subGameNames.indexOf(tmp.name)
+      if(selectedIndex<0) {
+        subGameNames.push(tmp.name)
+        subGames.push({
+          id: tmp.id,
+          name: tmp.name,
+          version: tmp.version,
+          description: tmp.description,
+          file_path: Env.get('SITE_URL') + tmp.file_path,
+          icon_path: Env.get('SITE_URL') + tmp.icon_path
+        })
+      }else {
+        if(subGames[selectedIndex].version < tmp.version) {
+          subGames[selectedIndex] =  {
+            id: tmp.id,
+            name: tmp.name,
+            version: tmp.version,
+            description: tmp.description,
+            file_path: Env.get('SITE_URL') + tmp.file_path,
+            icon_path: Env.get('SITE_URL') + tmp.icon_path
+          }
+        }
+      }
     }
+
     return [{
       status: 1,
       messages: [],
