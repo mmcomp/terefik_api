d71a66781156c3ee254aee4b2459f4b5ca132709
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2019-02-03 12:49:31 +0330

    disable zone check

--- app/Models/Hooks/ArrestZoneHook.js
+++ app/Models/Hooks/ArrestZoneHook.js
@@ -14,11 +14,13 @@ ArrestZoneHook.zoneDetect = async (rangerWork) => {
     let zones = await UserZone.query().where('users_id', rangerWork.ranger_id).pluck('zone_id')
     rangerWork.zone_id = 0
     if(zones.length>0) {
+        /*
         let query = "SELECT id FROM zone WHERE id in (" + zones.join(',') + ") and st_intersects(shape, point(" + rangerWork.lon_gps + ", " + rangerWork.lat_gps + "))=1"
         let res = await Database.raw(query)
         if(res[0].length>0) {
             rangerWork.zone_id = res[0][0].id
         }
+        */
     }
 
     let userProperty = await Property.query().where('user_id', rangerWork.ranger_id).first()
