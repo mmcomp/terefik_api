5a4c8c11ef7e5280edbc1a0cb80d87773823c1f8
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2019-03-02 09:41:02 +0330

    fix push notofocation

--- app/Models/Hooks/NotificationHook.js
+++ app/Models/Hooks/NotificationHook.js
@@ -8,6 +8,7 @@ const NotificationHook = exports = module.exports = {}
 
 NotificationHook.send = async (notification) => {
     let response
+    console.log('Sending PUSH')
     try{
         let data = {
             "applications": [Env.get('PUSH_PACKAGE')],
@@ -24,12 +25,25 @@ NotificationHook.send = async (notification) => {
             data['notification']['icon'] = Env.get('PUSH_USER_ARREST_ICON')
         }
         if(notification.users_id>0) {
+            console.log('User ID = ', notification.users_id)
             let theUser = await User.query().where('id', notification.users_id).first()
             if(theUser) {
+                console.log('Pushe Id ', theUser.pushe_id)
                 if(theUser.pushe_id && theUser.pushe_id!='') {
                     data['filter'] = {
                         pushe_id: [theUser.pushe_id]
                     }
+
+                    console.log('Push Data')
+                    console.log(data)
+                    axios.defaults.baseURL = Env.get('PUSH_URL')
+                    axios.defaults.timeout = 1000
+                    axios.defaults.headers.common['Authorization'] = 'Token ' + Env.get('PUSH_TOKEN')
+                    axios.defaults.headers.post['Content-Type'] = 'application/json'
+                    axios.defaults.headers.post['Accept'] = 'application/json'
+                    response = await axios.post('', data)
+                    console.log('Push response')
+                    console.log(response.data)
                 }
                 let pubTopic = 'client_' + theUser.token + '/ArrestNotification'
                 let data = {}
@@ -55,16 +69,6 @@ NotificationHook.send = async (notification) => {
                 return false
             }
         }
-        console.log('Push Data')
-        console.log(data)
-        axios.defaults.baseURL = Env.get('PUSH_URL')
-        axios.defaults.timeout = 1000
-        axios.defaults.headers.common['Authorization'] = 'Token ' + Env.get('PUSH_TOKEN')
-        axios.defaults.headers.post['Content-Type'] = 'application/json'
-        axios.defaults.headers.post['Accept'] = 'application/json'
-        response = await axios.post('', data)
-        console.log('Push response')
-        console.log(response.data)
         return response.data
     }catch(e) {
         return false
