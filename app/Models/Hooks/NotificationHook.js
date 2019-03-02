15cceafe2cebb0bbc2ef8df26a6db2a1d4af3bdf
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2019-03-02 11:19:25 +0330

    fix push notofocation sending

--- app/Models/Hooks/NotificationHook.js
+++ app/Models/Hooks/NotificationHook.js
@@ -7,44 +7,53 @@ const User = use('App/Models/User')
 const NotificationHook = exports = module.exports = {}
 
 NotificationHook.send = async (notification) => {
-    let response
-    console.log('Sending PUSH')
-    try{
-        let data = {
-            "applications": [Env.get('PUSH_PACKAGE')],
-            "notification": {
-            "title": notification.title,
-            "content": notification.message,
-            "action": {
-                "url": "",
-                "action_type": "A"
-            }
+    let response, pusheData = {
+        applications: [Env.get('PUSH_PACKAGE')],
+        notification: {
+            title: notification.title,
+            content: notification.message,
+            action: {
+                url: "",
+                action_type: "A"
             }
         }
+    }
+    console.log('Sending PUSH')
+    try{
         if(notification.type === 'user_arrest') {
-            data['notification']['icon'] = Env.get('PUSH_USER_ARREST_ICON')
+            pusheData.notification['icon'] = Env.get('PUSH_USER_ARREST_ICON')
         }
         if(notification.users_id>0) {
             console.log('User ID = ', notification.users_id)
             let theUser = await User.query().where('id', notification.users_id).first()
             if(theUser) {
                 console.log('Pushe Id ', theUser.pushe_id)
-                // if(theUser.pushe_id!='') {
-                    data['filter'] = {
+                if(theUser.pushe_id!='') {
+                    pusheData['filter'] = {
                         pushe_id: [theUser.pushe_id]
                     }
 
                     console.log('Push Data')
-                    console.log(data)
-                    axios.defaults.baseURL = Env.get('PUSH_URL')
-                    axios.defaults.timeout = 1000
-                    axios.defaults.headers.common['Authorization'] = 'Token ' + Env.get('PUSH_TOKEN')
-                    axios.defaults.headers.post['Content-Type'] = 'application/json'
-                    axios.defaults.headers.post['Accept'] = 'application/json'
-                    response = await axios.post('', data)
+                    console.log(pusheData)
+                    // axios.defaults.baseURL = Env.get('PUSH_URL')
+                    // axios.defaults.timeout = 1000
+                    // axios.defaults.headers.common['Authorization'] = 'Token ' + Env.get('PUSH_TOKEN')
+                    // axios.defaults.headers.post['Content-Type'] = 'application/json'
+                    // axios.defaults.headers.post['Accept'] = 'application/json'
+                    // response = await axios.post('', data)
+                    response = await axios({
+                        method: 'POST',
+                        headers: {
+                            'Authorization': 'Token ' + Env.get('PUSH_TOKEN'),
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                        },
+                        url : Env.get('PUSH_URL'), 
+                        data: pusheData,
+                    })
                     console.log('Push response')
                     console.log(response.data)
-                // }
+                }
                 let pubTopic = 'client_' + theUser.token + '/ArrestNotification'
                 let data = {}
                 try{
@@ -71,6 +80,8 @@ NotificationHook.send = async (notification) => {
         }
         return response.data
     }catch(e) {
+        console.log('Notification Send Hook Error')
+        console.log(e)
         return false
     }
 }
