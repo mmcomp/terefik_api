996026325443711a9d7c096c55ce5407965c15d4
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2019-03-25 14:55:37 +0430

    fix parkings around

--- app/Models/Parking.js
+++ app/Models/Parking.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const Database = use('Database')
+
 const Model = use('Model')
 
 class Parking extends Model {
   static async getParkingsAround(lon, lat, distance) {
     // let sql = "select `vehicle_id`,st_distance(point(lon, lat), point(" + lon + ", " + lat + ")) * 100000  `dis`, `shield_start`, `shield_duration`, `lon`, `lat` from `user_vehicle` where st_distance(point(lon, lat), point(" + lon + ", " + lat + ")) < " + String(parseFloat(distance)/100000)
-    let sql = "select `vehicle_id`,glength(LineStringFromWKB(LineString(GeomFromText(astext(PointFromWKB(POINT(lon,lat)))),GeomFromText(astext(PointFromWKB(POINT(" + lon + ", " + lat + "))))))) * 100000  `dis`, `lon`, `lat` from `parkings` where glength(LineStringFromWKB(LineString(GeomFromText(astext(PointFromWKB(POINT(lon,lat)))),GeomFromText(astext(PointFromWKB(POINT(" + lon + ", " + lat + "))))))) < " + String(parseFloat(distance)/100000);
+    let sql = "select glength(LineStringFromWKB(LineString(GeomFromText(astext(PointFromWKB(POINT(lon,lat)))),GeomFromText(astext(PointFromWKB(POINT(" + lon + ", " + lat + "))))))) * 100000  `dis`, `lon`, `lat`, `name`, `online_capacity`, `capacity` from `parkings` where glength(LineStringFromWKB(LineString(GeomFromText(astext(PointFromWKB(POINT(lon,lat)))),GeomFromText(astext(PointFromWKB(POINT(" + lon + ", " + lat + "))))))) < " + String(parseFloat(distance)/100000);
     console.log('Parkings Around Query')
     console.log(sql)
     let results = await Database.raw(sql)
